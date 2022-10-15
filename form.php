728124a0d33cd6a66dc5fda1dafd219f44cb46e1
DhruviThakkar210 <103808644+DhruviThakkar210@users.noreply.github.com>
2022-10-15 18:19:08 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+  /* kindly don't touch this area as it is linked with other php file*/
+<form method="post" action="insert.php">
+	<label>Full name</label><input type="text" name="uname"><br>
+	<label>Post</label><input type="text" name="post"><br>
+	<label>Mno</label><input type="text" name="mno"><br>
+	<label>PFP</label><input type="text" name="pfp"><br>
+	<input type="submit" name="submit" value="submit">
+  /*till here*/
+</form>
+</body>
+</html>
